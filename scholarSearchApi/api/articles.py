f5d934b3d648c4a0ea9f464151c8af8ab8ed1d69
nikkihekmat <nikkihekmat@gmail.com>
2024-02-09 13:49:51 -0800

    cleaning up article.py

--- scholarSearchApi/api/articles.py
+++ scholarSearchApi/api/articles.py
@@ -1,16 +1,5 @@
-"""
-These imports define the key objects
-"""
-
-
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-
-
-"""
-These object and definitions are used throughout the Jupyter Notebook.
-"""
-
 
 # Setup of key Flask object (app)
 app = Flask(__name__)
@@ -21,12 +10,8 @@ app.config['SQLALCHEMY_DATABASE_URI'] = database
 app.config['SECRET_KEY'] = 'SECRET_KEY'
 db = SQLAlchemy()
 
-
-
-
 # This belongs in place where it runs once per project
 db.init_app(app)
-
 
 """ database dependencies to support sqlite examples """
 import datetime
@@ -36,9 +21,6 @@ import json
 
 from sqlalchemy.exc import IntegrityError
 from werkzeug.security import generate_password_hash, check_password_hash
-
-
-
 
 ''' Tutorial: https://www.sqlalchemy.org/library.html#tutorials, try to get into a Python shell and follow along '''
 
@@ -59,21 +41,21 @@ class Article(db.Model):
    _link = db.Column(db.String(255), unique=True, nullable=False)
 
 
-   # constructor of a Article object, initializes the instance variables within object (article)
-   def __init__(article, title, author, link):
-       article._title = title   
-       article._author = author
-       article._link = link
+# constructor of a Article object, initializes the instance variables within object (article)
+def __init__(article, title, author, link):
+    article._title = title   
+    article._author = author
+    article._link = link
 
 
    # CRUD create/add a new record to the table
    # returns article or None on error
-   def create(article):
+def create(article):
        try:
-           # creates an article object from Article(db.Model) class, passes initializers
-           db.session.add(article)  # add prepares to persist person object to Users table
-           db.session.commit()  # SqlAlchemy "unit of work pattern" requires a manual commit
-           return article
+        # creates an article object from Article(db.Model) class, passes initializers
+        db.session.add(article)  # add prepares to persist person object to Users table
+        db.session.commit()  # SqlAlchemy "unit of work pattern" requires a manual commit
+        return article
        except IntegrityError:
            db.session.remove()
            return None
@@ -81,8 +63,8 @@ class Article(db.Model):
 
    # CRUD read converts self to dictionary
    # returns dictionary
-   def read(article):
-       return {
+def read(article):
+    return {
            "id": article.id,
            "title": article.title,
            "author": article.author,
@@ -90,42 +72,40 @@ class Article(db.Model):
        }
 
 
-   # CRUD delete: remove article
-   # None
-   def delete(article):
-       db.session.delete(article)
-       db.session.commit()
-       return None
+# CRUD delete: remove article
+# None
+def delete(article):
+    db.session.delete(article)
+    db.session.commit()
+    return None
   
 # Database Creation and Testing
 
 
 # Builds working data for testing
+
 def initArticles():
-   with app.app_context():
-       db.create_all()
-       u1 = Article(title='12 Strategies to Writing the Perfect College Essay', author='Pamela Reynolds', link='https://summer.harvard.edu/blog/12-strategies-to-writing-the-perfect-college-essay/')
-       u2 = Article(title='Nikola Tesla', author='niko', link='123niko')
-       u3 = Article(title='Alexander Graham Bell', uid='lex', link='123lex')
-       u4 = Article(title='Eli Whitney', author='whit', link='123whit')
-       u5 = Article(title='Indiana Jones', author='indi', link='')
-       u6 = Article(title='Marion Ravenwood', author='raven', link='')
+    with app.app_context():
+        db.create_all()
+        u1 = Article(title='12 Strategies to Writing the Perfect College Essay', author='Pamela Reynolds', link='https://summer.harvard.edu/blog/12-strategies-to-writing-the-perfect-college-essay/')
+        u2 = Article(title='Nikola Tesla', author='niko', link='123niko')
+        u3 = Article(title='Alexander Graham Bell', uid='lex', link='123lex')
+        u4 = Article(title='Eli Whitney', author='whit', link='123whit')
+        u5 = Article(title='Indiana Jones', author='indi', link='')
+        u6 = Article(title='Marion Ravenwood', author='raven', link='')
+
+    articles = [u1, u2, u3, u4, u5, u6]
 
 
-
-
-       articles = [u1, u2, u3, u4, u5, u6]
-
-
-       """Builds sample user/note(s) data"""
-       for article in articles:
-           try:
-               '''add article to table'''
-               object = article.create()
-               print(f"Created new uid {object.uid}")
-           except:  # error raised if object nit created
-               '''fails with bad or duplicate data'''
-               print(f"Records exist uid {article.uid}, or error.")
+"""Builds sample user/note(s) data"""
+for article in articles:
+    try:
+        '''add article to table'''
+        object = article.create()
+        print(f"Created new uid {object.uid}")
+    except:  # error raised if object nit created
+        '''fails with bad or duplicate data'''
+        print(f"Records exist uid {article.uid}, or error.")
               
 initUsers()
 
@@ -139,7 +119,8 @@ def find_by_uid(uid):
 
 # Check credentials by finding user and verify password
 def check_credentials(uid, password):
-   # query email and return user record
+
+# query email and return user record
    user = find_by_uid(uid)
    if user == None:
        return False
@@ -166,17 +147,17 @@ def create():
    password = input("Enter your password")
   
    # Initialize User object before date
-   user = User(name=name,
+   user = Article(name=name,
                uid=uid,
                password=password
                )
   
    # create user.dob, fail with today as dob
-   dob = input("Enter your date of birth 'YYYY-MM-DD'")
-   try:
-       user.dob = datetime.strptime(dob, '%Y-%m-%d').date()
-   except ValueError:
-       user.dob = datetime.today()
+dob = input("Enter your date of birth 'YYYY-MM-DD'")
+    try:
+       article.dob = datetime.strptime(dob, '%Y-%m-%d').date()
+    except ValueError:
+       article.dob = datetime.today()
        print(f"Invalid date {dob} require YYYY-mm-dd, date defaulted to {user.dob}")
          
    # write object to database
